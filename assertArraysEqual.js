73bbc21042e873a22ee56a59ac0d03a11f98362f
Jeff Bridges <jbridges1119@gmail.com>
2022-06-01 00:46:17 +0000

    Changed to eqArrays is leveraged into assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,18 @@
-const assertArraysEqual = function(actual, expected) {
+const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   }
 
   for (let i = 0; i < expected.length; i++) {
     if (actual[i] !== expected[i]) {
-      return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+      return false;
     }
-  } return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
