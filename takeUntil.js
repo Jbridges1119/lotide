d41bcdd791383e1f67501d86105556e85ffea648
Jeff Bridges <jbridges1119@gmail.com>
2022-06-02 23:12:49 +0000

    wrote a higher order function and tested.

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < expected.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let value of array) {
+    if (callback(value)) {
+      return result;
+    } else {
+      result.push(value);
+    }
+  } return result;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
