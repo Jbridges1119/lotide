1ba264b38e43d116f9cb6ec9f723f9187191adbd
Jeff Bridges <jbridges1119@gmail.com>
2022-06-02 13:53:37 +0000

    Corrected format

--- countLetters.js
+++ countLetters.js
@@ -8,13 +8,14 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(sentence) {
   let results = {};
   for (let letter of sentence) {
-    if(letter !== " ")
-    if (results[letter]) {
-      results[letter] += 1;
-    } else {
-      results[letter] = 1;
+    if (letter !== " "); {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
     }
   } return console.log(results);
 };
 
-countLetters("lighthouse in the house")
+countLetters("lighthouse in the house");
