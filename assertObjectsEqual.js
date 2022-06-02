10c58d719a817a86bad84f79295561f7f57518f0
Jeff Bridges <jbridges1119@gmail.com>
2022-06-02 02:11:51 +0000

    created function to console.log a pass fail response for object matching

---
+++ assertObjectsEqual.js
@@ -0,0 +1,46 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < expected.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const eqObjects = function(obj1, obj2) {
+  const keysObj1 = Object.keys(obj1);
+  const keysObj2 = Object.keys(obj2);
+  if (keysObj1.length !== keysObj2.length) {
+    return false;
+  }
+
+  for (let key of keysObj1) {
+    if (Array.isArray(obj1[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
+    } else {
+      if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+  } return true;
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    return console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  }
+  return console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: "2", c: "1" };
+assertObjectsEqual(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(eqObjects(cd, cd2), true); // => false
