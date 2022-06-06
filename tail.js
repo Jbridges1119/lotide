a67763fd6120101f7bf88c3a2b1a6946cefe3a3a
Jeff Bridges <jbridges1119@gmail.com>
2022-06-06 14:48:44 +0000

    Refeactored tail and created tailTest as testing file

--- tail.js
+++ tail.js
@@ -1,19 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = array;
   return newArray.slice(1);
 };
-const result = tail([]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+
+module.exports = tail;
